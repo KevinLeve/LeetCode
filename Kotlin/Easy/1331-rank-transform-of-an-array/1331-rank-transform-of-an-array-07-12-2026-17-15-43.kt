0e9ef3b793911fa7e803f3ddf2b0e534d1b590eb
Kevin B <kevinleve@yahoo.com>
2026-07-12 17:15:36 +0530

    103 ms (25%) 63.6 MB (81.25%) 07-12-2026 1331-rank-transform-of-an-array Easy .kt Array

---
+++ Kotlin/Easy/1331-rank-transform-of-an-array/1331-rank-transform-of-an-array-07-12-2026-17-15-43.kt
@@ -0,0 +1,19 @@
+class Solution {
+    // Complexity:
+    // Time O(NLogN) and Space O(N) where N is the length of arr.
+    fun arrayRankTransform(arr: IntArray): IntArray {
+        if (arr.isEmpty()) {
+            return intArrayOf()
+        }
+
+        val indices = arr.indices.sortedBy { arr[it] }
+        val rank = IntArray(arr.size)
+        rank[indices[0]] = 1
+        for (i in 1..<indices.size) {
+            rank[indices[i]] =
+                rank[indices[i - 1]] + (if (arr[indices[i]] == arr[indices[i - 1]]) 0 else 1)
+        }
+
+        return rank
+    }
+}
