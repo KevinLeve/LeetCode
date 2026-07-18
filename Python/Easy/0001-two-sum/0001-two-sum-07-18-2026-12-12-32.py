73bf5ca623719b674198b04d7d11933492a25281
Kevin B <kevinleve@yahoo.com>
2026-07-18 12:12:31 +0530

    0 ms (100%) 13 MB (67.32%) 07-18-2026 0001-two-sum Easy .py Array

---
+++ Python/Easy/0001-two-sum/0001-two-sum-07-18-2026-12-12-32.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        mp = {}
+
+        for i in xrange(len(nums)):
+            need = target - nums[i]
+
+            if need in mp:
+                return [mp[need], i]
+
+            mp[nums[i]] = i
