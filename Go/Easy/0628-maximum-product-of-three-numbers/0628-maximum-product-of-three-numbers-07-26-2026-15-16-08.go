99194631d45af58653c555712b620f3a9f455308
Kevin B <kevinleve@yahoo.com>
2026-07-26 15:15:57 +0530

    8 ms (37.68%) 8.8 MB (27.54%) 07-26-2026 0628-maximum-product-of-three-numbers Easy .go Array

---
+++ Go/Easy/0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers-07-26-2026-15-16-08.go
@@ -0,0 +1,16 @@
+import "sort"
+
+func maximumProduct(nums []int) int {
+	sort.Ints(nums)
+
+	n := len(nums)
+
+	a := nums[n-1] * nums[n-2] * nums[n-3]
+	b := nums[0] * nums[1] * nums[n-1]
+
+	if a > b {
+		return a
+	}
+
+	return b
+}
