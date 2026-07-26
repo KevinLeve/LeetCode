3faf60d173442ed4260a9481b1441c7f34b54b6a
Kevin B <kevinleve@yahoo.com>
2026-07-26 15:17:15 +0530

    26 ms (40%) 21.9 MB (40%) 07-26-2026 0628-maximum-product-of-three-numbers Easy .php Array

---
+++ PHP/Easy/0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers-07-26-2026-15-17-26.php
@@ -0,0 +1,17 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function maximumProduct($nums) {
+        sort($nums);
+
+        $n = count($nums);
+
+        return max(
+            $nums[$n - 1] * $nums[$n - 2] * $nums[$n - 3],
+            $nums[0] * $nums[1] * $nums[$n - 1]
+        );
+    }
+}
