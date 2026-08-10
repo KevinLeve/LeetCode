bbd8214d49f93a087a445a35ed16d0f594a689f7
Kevin B <kevinleve@yahoo.com>
2026-08-10 11:40:42 +0530

    18 ms (100%) 150.7 MB (100%) 08-10-2026 1510-stone-game-iv Hard .dart Math

---
+++ Dart/Hard/1510-stone-game-iv/1510-stone-game-iv-08-10-2026-11-41-20.dart
@@ -0,0 +1,16 @@
+class Solution {
+  bool winnerSquareGame(int n) {
+    List<bool> dp = List.filled(n + 1, false);
+
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j * j <= i; j++) {
+        if (!dp[i - j * j]) {
+          dp[i] = true;
+          break;
+        }
+      }
+    }
+
+    return dp[n];
+  }
+}
