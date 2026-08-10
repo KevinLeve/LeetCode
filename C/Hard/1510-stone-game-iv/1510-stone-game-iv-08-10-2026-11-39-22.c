af51b602fbc4a9ef9d80645532ab33e64b6066de
Kevin B <kevinleve@yahoo.com>
2026-08-10 11:38:44 +0530

    22 ms (18.18%) 9.2 MB (36.36%) 08-10-2026 1510-stone-game-iv Hard .c Math

---
+++ C/Hard/1510-stone-game-iv/1510-stone-game-iv-08-10-2026-11-39-22.c
@@ -0,0 +1,20 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+bool winnerSquareGame(int n) {
+    bool *dp = calloc(n + 1, sizeof(bool));
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j * j <= i; j++) {
+            if (!dp[i - j * j]) {
+                dp[i] = true;
+                break;
+            }
+        }
+    }
+
+    bool result = dp[n];
+    free(dp);
+
+    return result;
+}
